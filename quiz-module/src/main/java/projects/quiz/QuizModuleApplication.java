09d384f927b0e8f014f057990665658ad340b82a
Piotr <piotr.szymanski@itsilesia.com>
2020-01-07 23:15:38 +0100

    Added empty quiz module

---
+++ quiz-module/src/main/java/projects/quiz/QuizModuleApplication.java
@@ -0,0 +1,15 @@
+package projects.quiz;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizModuleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(QuizModuleApplication.class, args);
+    }
+
+}
